ad34f54c93a185f320dd010dd1bb8bd29656959b
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-08-25 09:39:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById('main')
+main.remove('main')
+const newHeader = document.createElement('h1')
+//document.body.append(newHeader)
+newHeader.setAttribute('id', 'victory')
+//newHeader = document.querySelector('h1#victory')
+//console.log(newHeader)
+newHeader.innerHTML = 'YOUR-NAME is the champion'
